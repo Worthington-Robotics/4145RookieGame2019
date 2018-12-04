f43cb70eaf01761c1a8df8c88dcfef4d2213fb21
AIDAN-SCHNEIDER <45183337+AIDAN-SCHNEIDER@users.noreply.github.com>
2018-12-04 17:50:41 -0500

    Create zuccDrive.java

---
+++ src/main/java/frc/robot/subsystems/zuccDrive.java
@@ -0,0 +1,63 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.lib.loops.Loop;
+import frc.lib.util.DriveSignal;
+import frc.lib.util.HIDHelper;
+import frc.robot.Constants;
+
+public class zuccDrive extends Subsystem {
+
+
+    private static final zuccDrive ZucC = new zuccDrive();
+
+    private TalonSRX frontleft, frontright, rearleft, rearright;
+    private double leftsignal = 0, rightsignal = 0;
+
+    public static zuccDrive getInstance() {
+        return ZucC;
+    }
+
+    private final Loop zuccloop = new Loop() {
+        @Override
+        public void onStart(double timestamp) {
+
+        }
+
+        @Override
+        public void onLoop(double timestamp) {
+
+        }
+
+        @Override
+        public void onStop(double timestamp) {
+
+        }
+    };
+
+    @Override
+    public void readPeriodicInputs() {
+    }
+
+    @Override
+    public void writePeriodicOutputs() {
+
+    }
+
+    @Override
+    public void outputTelemetry() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
