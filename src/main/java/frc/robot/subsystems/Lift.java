2f24aa2d397c21fa56f7a58057bf67f69293bc1e
Milo <pamimoo@users.noreply.github.com>
2018-12-11 17:26:43 -0500

    Update Lift.java

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -1,12 +1,39 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Spark;
+import frc.lib.loops.Loop;
+import frc.lib.util.HIDHelper;
+import frc.robot.Constants;
 
 public class Lift extends Subsystem{
     private Spark lift1, lift2;
+
     public Lift () {
         lift1 = new Spark(1);
         lift2 = new Spark(2);
+    }
+
+    private final Loop liftloop;
+
+    {
+        liftloop = new Loop() {
+
+            @Override
+            public void onStart(double timestamp) {
+
+            }
+
+            @Override
+            public void onLoop(double timestamp) {
+                double[] liftStick = HIDHelper.getAdjStick(Constants.SECOND_STICK);
+                
+            }
+
+            @Override
+            public void onStop(double timestamp) {
+
+            }
+        };
     }
 
     @Override
