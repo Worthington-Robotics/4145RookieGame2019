41ae87531c16d03f65f60307b6f02bdedf3e15c8
MattSavage1340 <45183240+MattSavage1340@users.noreply.github.com>
2018-12-14 19:04:35 -0500

    SavageAutoSimpler

--- src/main/java/frc/robot/AutoModes/SavageAuto.java
+++ src/main/java/frc/robot/AutoModes/SavageAuto.java
@@ -11,12 +11,11 @@ import frc.robot.actions.LiftAction;
 public class SavageAuto extends StateMachineDescriptor {
 
     public SavageAuto() {
-        addParallel(new Action[]{new LiftAction(1), new DriveAction(.75, .75)},1000);
-        addSequential(new DriveAction(.5, 0), 1000);
-        addSequential(new DriveAction (.5,.5),1000 );
-        addSequential(new DriveAction(0,.5),1000);
-        addSequential(new DriveAction(.5,.5), 500);
-        addSequential(new ForkAction(ForkAction.ShotPower.Shoot),1000);
+        addSequential(new LiftAction(.75), 100);
+        addSequential(new DriveAction(.35,.35),2500);
+        addSequential(new LiftAction(.5),100);
+        addSequential(new ForkAction(ForkAction.ShotPower.Shoot),100);
+
 
 
 
